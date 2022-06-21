b73b8a026ac148ecea49aafd5618a9e32f7f41b8
agusvalori <61027212+agusvalori@users.noreply.github.com>
2022-06-21 15:39:21 -0300

    Viendo si logro modificar el github

--- src/data/AlumnoData.java
+++ src/data/AlumnoData.java
@@ -3,10 +3,17 @@
  */
 package data;
 
+import java.sql.Connection;
+
 /**
  *
  * @author Grupo3_LabI
  */
 public class AlumnoData {
+    private Connection conn = null;
+
+    public AlumnoData() {
+        
+    }
     
 }
